f9959cccaf972bb2dc3cb5f1f680783c89ca3a8d
Jaquavia <walkertechdesigns@gmail.com>
2021-02-28 20:33:21 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// const navslide = () => {
+// 	const hamburger = document.querySelector('.hamburger');
+// 	const nav = document.querySelector('.nav-links');
+
+// 	hamburger.addEventListener('click', () => {
+// 		nav.classList.toggle('nav-links');
+// 	});
+// };
+
+// navslide();
+
+// document.getElementById('demo').addEventListener('mouseover', mouseOver);
+// document.getElementById('demo').addEventListener('mouseout', mouseOut);
+
+// function mouseOver() {
+// 	document.getElementById('demo').textContent = 'Scroll Down';
+// }
+
+// function mouseOut() {
+// 	document.getElementById('demo').textContent = '';
+// }
