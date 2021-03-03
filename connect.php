aee0c0a34b25662eb110edd6bf1c486f562c3ad7
Jaquavia <walkertechdesigns@gmail.com>
2021-03-03 08:40:58 -0500

    inital php commit

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php 
+  $firstName = $_POST ['firstName'];
+  $lastName = $_POST ['lastName'];
+  $email = $_POST ['email'];
+  $subject = $_POST ['subject'];
+  $message = $_POST ['message'];
+?> 
